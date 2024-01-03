b2ff90840520c48ef0d2501888eaaa6a6f54775c
coder12git <suruchikumarimfp4@gmail.com>
2024-01-03 12:40:55 +0530

    location feature
    Signed-off-by: coder12git <suruchikumarimfp4@gmail.com>

--- tauri-app/src/pages/nearbyDoctor.tsx
+++ tauri-app/src/pages/nearbyDoctor.tsx
@@ -1,28 +1,28 @@
 import { FunctionComponent, useEffect, useRef, useState } from "react";
 import useWeb5Store from "@/stores/useWeb5Store";
-import { fetch, ResponseType } from '@tauri-apps/api/http';
-import axios from 'axios'
+// import { fetch, ResponseType } from '@tauri-apps/api/http';
+// import axios from 'axios'
 
-const accessToken = 'AIzaSyCfqty5e_JIBd-2RrdbB0W5ykv3D4noYNg'
+const accessToken = "AIzaSyCfqty5e_JIBd-2RrdbB0W5ykv3D4noYNg"
 
 const Doctors: FunctionComponent = () => {
     const { web5 } = useWeb5Store((state) => ({ web5: state.web5!, connect: state.connect }));
-    const [places, setPlaces] = useState(null);
+    const [places, setPlaces] = useState<any[]>([]);
 
     useEffect(()=>{
         const fetchData = async()=>{
             try {
-                const response = await axios.get(
-                  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&keyword=hospital&key=${accessToken}`,
+                const response = await fetch(
+                  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=22.572645%2C88.363892&radius=1500&keyword=hospital&key=${accessToken}`
                 );
         
-                if (!response.data.ok) {
+                if (!response.ok) {
                   throw new Error('Network response was not ok');
                 }
         
-                // const result = response.data;
-                // console.log(result);
-                setPlaces(response.data.json);
+                const result = await response.json();
+                console.log(result.results)
+                setPlaces(result.results);
               } catch (error) {
                 console.error('Error fetching data:', error);
               }
@@ -33,7 +33,15 @@ const Doctors: FunctionComponent = () => {
     },[web5])
   return (
     <div>
-        {places}
+        {places?.map((place)=>(
+          <div key={place.place_id}>
+            <ul>
+               <li className="font-bold">Name: {place.name}</li>
+               <li>Address: {place.vicinity}</li>
+               <li>Rating: {place.rating}</li>
+            </ul>
+          </div>
+        ))}
     </div>
   )
 }
