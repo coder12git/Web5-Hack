8ac89fbf331c5c2bb8a5fb5dc4124534f99f445e
coder12git <suruchikumarimfp4@gmail.com>
2024-01-03 12:53:28 +0530

    nearbyDoctors feature done
    Signed-off-by: coder12git <suruchikumarimfp4@gmail.com>

--- tauri-app/src/pages/nearbyDoctor.tsx
+++ tauri-app/src/pages/nearbyDoctor.tsx
@@ -8,12 +8,25 @@ const accessToken = "AIzaSyCfqty5e_JIBd-2RrdbB0W5ykv3D4noYNg"
 const Doctors: FunctionComponent = () => {
     const { web5 } = useWeb5Store((state) => ({ web5: state.web5!, connect: state.connect }));
     const [places, setPlaces] = useState<any[]>([]);
+    const [lat, setlat] = useState<number>();
+    const [long, setlong] = useState<number>();
+
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position)=>{
+        const lat = position.coords.latitude
+        const long = position.coords.longitude
+        setlat(lat);
+        setlong(long);
+      })
+    } else {
+      console.log('Geolocation is not supported')
+    }
 
     useEffect(()=>{
         const fetchData = async()=>{
             try {
                 const response = await fetch(
-                  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=22.572645%2C88.363892&radius=1500&keyword=hospital&key=${accessToken}`
+                  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&keyword=hospital&key=${accessToken}`
                 );
         
                 if (!response.ok) {
