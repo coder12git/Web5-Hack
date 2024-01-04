46ff71112a930054578e3261cceb7cb3e66849b7
coder12git <suruchikumarimfp4@gmail.com>
2024-01-04 11:17:48 +0530

    make nearbyDoctor page to track location based on user input
    Signed-off-by: coder12git <suruchikumarimfp4@gmail.com>

--- tauri-app/src/pages/nearbyDoctor.tsx
+++ tauri-app/src/pages/nearbyDoctor.tsx
@@ -1,18 +1,17 @@
 import { FunctionComponent, useEffect, useRef, useState } from "react";
 import useWeb5Store from "@/stores/useWeb5Store";
-// import { fetch, ResponseType } from '@tauri-apps/api/http';
-// import axios from 'axios'
 
 const accessToken = "AIzaSyCfqty5e_JIBd-2RrdbB0W5ykv3D4noYNg"
 
 const Doctors: FunctionComponent = () => {
-    const { web5 } = useWeb5Store((state) => ({ web5: state.web5!, connect: state.connect }));
-    const [places, setPlaces] = useState<any[]>([]);
-    const [lat, setlat] = useState<number>();
-    const [long, setlong] = useState<number>();
+  const { web5 } = useWeb5Store((state) => ({ web5: state.web5!, connect: state.connect }));
+  const [places, setPlaces] = useState<any[]>([]);
+  const [lat, setlat] = useState<number>();
+  const [long, setlong] = useState<number>();
 
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition((position)=>{
+  const findHospitals = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude
         const long = position.coords.longitude
         setlat(lat);
@@ -22,39 +21,72 @@ const Doctors: FunctionComponent = () => {
       console.log('Geolocation is not supported')
     }
 
-    useEffect(()=>{
-        const fetchData = async()=>{
-            try {
-                const response = await fetch(
-                  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&keyword=hospital&key=${accessToken}`
-                );
-        
-                if (!response.ok) {
-                  throw new Error('Network response was not ok');
-                }
-        
-                const result = await response.json();
-                console.log(result.results)
-                setPlaces(result.results);
-              } catch (error) {
-                console.error('Error fetching data:', error);
-              }
-            };
-        if (web5) {
-            fetchData()
-          }
-    },[web5])
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&keyword=hospital&key=${accessToken}`
+        );
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        console.log(result.results)
+        setPlaces(result.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    if (web5) {
+      fetchData()
+    }
+  }
+  // if(navigator.geolocation){
+  //   navigator.geolocation.getCurrentPosition((position)=>{
+  //     const lat = position.coords.latitude
+  //     const long = position.coords.longitude
+  //     setlat(lat);
+  //     setlong(long);
+  //   })
+  // } else {
+  //   console.log('Geolocation is not supported')
+  // }
+
+  // useEffect(()=>{
+  //     const fetchData = async()=>{
+  //         try {
+  //             const response = await fetch(
+  //               `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1500&keyword=hospital&key=${accessToken}`
+  //             );
+
+  //             if (!response.ok) {
+  //               throw new Error('Network response was not ok');
+  //             }
+
+  //             const result = await response.json();
+  //             console.log(result.results)
+  //             setPlaces(result.results);
+  //           } catch (error) {
+  //             console.error('Error fetching data:', error);
+  //           }
+  //         };
+  //     if (web5) {
+  //         fetchData()
+  //       }
+  // },[web5])
   return (
     <div>
-        {places?.map((place)=>(
-          <div key={place.place_id}>
-            <ul>
-               <li className="font-bold">Name: {place.name}</li>
-               <li>Address: {place.vicinity}</li>
-               <li>Rating: {place.rating}</li>
-            </ul>
-          </div>
-        ))}
+      <button onClick={findHospitals}>Find Hospitals</button>
+      {places?.map((place) => (
+        <div key={place.place_id}>
+          <ul>
+            <li className="font-bold">Name: {place.name}</li>
+            <li>Address: {place.vicinity}</li>
+            <li>Rating: {place.rating}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   )
 }
